784d29973c02fdbfccfa78a8e9f94f895e1932d9
Sy Rashid <sy.rashid.11@gmail.com>
2021-03-04 22:44:40 -0500

    FEAT: Added refreshing counter

--- app/javascript/controllers/counter_controller.js
+++ app/javascript/controllers/counter_controller.js
@@ -3,11 +3,16 @@ import { Controller } from "stimulus";
 export default class extends Controller {
   static targets = [ 'count' ];
 
-  refresh() {
+  connect() {
+    setInterval(this.refresh, 5000);
+  }
+
+  refresh = () => {
+    console.log("I'm being refreshed!")
     fetch("/restaurants", { headers: { accept: "application/json" } })
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      this.countTarget.innerText = data.restaurants.length;
     });
   }
 }
